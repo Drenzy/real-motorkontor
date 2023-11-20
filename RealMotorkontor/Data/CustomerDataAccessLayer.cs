a001b8f9b6ff0f122f6bc535ac82937cb1f57fef
Daniel Hartwich <dhartwich650@gmail.com>
2023-11-20 19:33:05 +0100

    ReaLMotokontoDone
    Done

---
+++ RealMotorkontor/Data/CustomerDataAccessLayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace RealMotorkontor.Data
+{
+    public class CustomerService
+    {
+        CustomerDataAccessLayer objCustomerDAL { get; set; } = new();
+
+
+        public string Create(Customer objCustomer)
+        {
+            objCustomerDAL.AddCustomer(objCustomer);
+            return "Added Successfully";
+        }
+
+        public List<Customer> GetCustomer()
+        {
+            List<Customer> customers = objCustomerDAL.GetAllCustomers().ToList();
+            return customers;
+        }
+
+        public Customer GetCustomerByID(int id)
+        {
+            Customer customer = objCustomerDAL.GetCustomerData(id);
+            return customer;
+        }
+        public string UpdateCustomer(Customer objcustomer)
+        {
+            objCustomerDAL.UpdateCustomer(objcustomer);
+            return "Update Successfully";
+        }
+        public string DeleteCustomer(Customer objcustomer)
+        {
+            objCustomerDAL.DeleteCustomer(objcustomer.CustomerId);
+            return "Delete Successfully";
+        }
+    }
+}
---
+++ RealMotorkontor/Data/CustomerService.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace RealMotorkontor.Data
+{
+    public class CustomerDataAccessLayer
+    {
+        // string connectionString = "Put Your Connection string here";
+        string connectionString = "Data Source=Daniels-LapTop;Initial Catalog=MotorKontorOpg;Integrated Security=True";
+        //To View all Customers details    
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            List<Customer> lstCustomer = new List<Customer>();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("usp_GetAllCustomers", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Customer Customer = new Customer();
+                    Customer.CustomerId = Convert.ToInt32(rdr["Customer_id"]);
+                    Customer.Name = rdr["Fname"].ToString();
+                    Customer.Gender = rdr["Gender"].ToString();
+                    Customer.Country = rdr["Country"].ToString();
+                    Customer.City = rdr["City"].ToString();
+                    lstCustomer.Add(Customer);
+                }
+                con.Close();
+            }
+            return lstCustomer;
+        }
+        //To Add new Customer record    
+        public void AddCustomer(Customer Customer)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("usp_AddCustomer", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Name", Customer.Name);
+                cmd.Parameters.AddWithValue("@Gender", Customer.Gender);
+                cmd.Parameters.AddWithValue("@Country", Customer.Country);
+                cmd.Parameters.AddWithValue("@City", Customer.City);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        //To Update the records of a particluar Customer  
+        /*
+        public void UpdateCustomer(Customer Customer)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("usp_UpdateCustomer", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@CustomerId", Customer.CustomerId);
+                cmd.Parameters.AddWithValue("@Name", Customer.Name);
+                cmd.Parameters.AddWithValue("@Gender", Customer.Gender);
+                cmd.Parameters.AddWithValue("@Country", Customer.Country);
+                cmd.Parameters.AddWithValue("@City", Customer.City);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        */
+        //Get the details of a particular Customer  
+        public Customer GetCustomerData(int? id)
+        {
+            Customer Customer = new Customer();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+
+                SqlCommand cmd = new SqlCommand("usp_GetCustomerByID", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@CustomerId", id);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Customer.CustomerId = Convert.ToInt32(rdr["CustomerID"]);
+                    Customer.Name = rdr["Name"].ToString();
+                    Customer.Gender = rdr["Gender"].ToString();
+                    Customer.Country = rdr["Country"].ToString();
+                    Customer.City = rdr["City"].ToString();
+                }
+            }
+            return Customer;
+        }
+        //To Delete the record on a particular Customer  
+       /* public void DeleteCustomer(int? id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("usp_DeleteCustomer", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@CustomerId", id);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+       */
+    }
+}
